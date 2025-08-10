654c1062958e7c860f4330c1ef32f6ee91e81171
dongfan <dongfans87@gmail.com>
2025-08-10 19:19:17 +0900

    push

--- src/opencv_tuto/opencv_tuto/img_pub.py
+++ src/opencv_tuto/opencv_tuto/img_pub.py
@@ -8,13 +8,29 @@ class ImagePublisher(Node):
     def __init__(self):
         super().__init__('img_publisher')
         self.publisher_ = self.create_publisher(Image, 'image_raw', 10)
-        time_period = 0.01
-        self.timer = self.create_timer(time_period, self.timer_callback)  # 10 Hz
-        self.cap = cv2.VideoCapture(0)
-        self.cv_bridge = CvBridge()
+        
+        self.declare_parameter('width', 640)
+        self.width = self.get_parameter('width').value
+        self.declare_parameter('height', 480)
+        self.height = self.get_parameter('height').value
+        self.declare_parameter('camera_device', 0)
+        self.camera_device = self.get_parameter('camera_device').value
+        self.declare_parameter('frame_rate', 480)
+        self.frame_rate = self.get_parameter('frame_rate').value
 
-    def timer_callback(self):
+        self.cap = cv2.VideoCapture(self.camera_device)
+        self.cv_bridge = CvBridge()
+        time_period = 1.0 / self.frame_rate
+        self.timer = self.create_timer(time_period, self.time_callback)
+
+        self.get_logger().info("Video Width : " + str(self.width))
+        self.get_logger().info("Video Height : " + str(self.height))
+        self.get_logger().info("Video Camera Device : " + str(self.camera_device))
+        self.get_logger().info("Video Frame Rate : " + str(self.frame_rate))
+
+    def time_callback(self):
         ret, frame = self.cap.read()
+        frame = cv2.resize(frame, (self.width, self.height))
         img = self.cv_bridge.cv2_to_imgmsg(frame, encoding='bgr8')
         self.publisher_.publish(img)
         cv2.waitKey(1)
